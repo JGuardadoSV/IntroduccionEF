c9e111beaee560bbf0269c69172de8a0cfb80337
Josué Guardado <jguardadosv@outlook.com>
2021-04-15 08:53:39 -0600

    Agregar archivos de proyecto.

---
+++ Introduccion EF/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion_EF
+{
+    public class Alumno
+    {
+        public int AlumnoId { get; set; } //Primary Key y propiedad Identity
+        public string Nombre { get; set; }
+        
+    }
+
+}
---
+++ Introduccion EF/introduccionEF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion_EF
+{
+    //Contexto, se debe de llamar igual que la cadena de conexión
+    public class introduccionEF:DbContext
+    {
+
+    }
+}
