354d56c3fd98ac0083e567e8e56845bcffa0f547
Josué Guardado <jguardadosv@outlook.com>
2021-04-22 08:40:13 -0600

    consulta con linq

--- Introduccion EF/Program.cs
+++ Introduccion EF/Program.cs
@@ -13,20 +13,37 @@ namespace Introduccion_EF
 
             //INSERT
 
+            /** // quitar comentarios para probar esta porcion de codigo
             Alumno alumno = new Alumno();
             alumno.Nombre = "Josue Guardado";
             Console.WriteLine("El ID antes de guardar {0}", alumno.AlumnoId);
 
             introduccionEF db = new introduccionEF();
             
+            // two ways binding
 
             db.Alumnos.Add(alumno); //Insert
 
             db.SaveChanges(); //Envia los cambios / consultas
 
             Console.WriteLine("El ID despues de guardar {0}", alumno.AlumnoId);
+            */
 
+            //SELECT
+            introduccionEF db = new introduccionEF();
+            //LINQ Lenguaje de consulta .net
+            //Expresiones Lambdas
 
+            // Un alumno
+            Alumno alumno = new Alumno();
+
+            alumno = (
+                          from i in db.Alumnos 
+                          where i.AlumnoId == 3 
+                          select i
+                      ).First();
+
+            Console.WriteLine("El nombre del alumno es {0} y id {1}", alumno.Nombre,alumno.AlumnoId);
 
         }
     }
